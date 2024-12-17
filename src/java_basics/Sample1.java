ca58a7a21eb561c87e7433b54a551aa8ce50e5f1
kowshick12B <kowshick12@gmail.com>
2024-12-17 11:05:41 +0530

    my first commite

---
+++ src/java_basics/Sample1.java
@@ -0,0 +1,18 @@
+package java_basics;
+
+public class Sample1 {
+
+	public static void m1()
+	{
+	  System.out.println("Git_tutorial");
+	  System.out.println("Github tutorial");
+	  System.out.println("Java");
+	  System.out.println("Selenium");
+	}
+
+	public static void main(String[] args) {
+	 
+	  m1();
+	}
+
+}
